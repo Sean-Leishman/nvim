c95ad7a897ce133ac7e8c70eef65a2c5a329edff
Sean Leishman <leishmansean@gmail.com>
2023-10-03 13:40:04 +0100

    Update README.md

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/seanleishman/.cache/nvim/packer_hererocks/2.1.1694316387/share/lua/5.1/?.lua;/home/seanleishman/.cache/nvim/packer_hererocks/2.1.1694316387/share/lua/5.1/?/init.lua;/home/seanleishman/.cache/nvim/packer_hererocks/2.1.1694316387/lib/luarocks/rocks-5.1/?.lua;/home/seanleishman/.cache/nvim/packer_hererocks/2.1.1694316387/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/seanleishman/.cache/nvim/packer_hererocks/2.1.1694316387/lib/lua/5.1/?.so"
+local package_path_str = "/home/seanleishman/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/seanleishman/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/seanleishman/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/seanleishman/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/seanleishman/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -90,7 +90,7 @@ _G.packer_plugins = {
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   embark = {
-    config = { "\27LJ\2\n6\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\23colorscheme embark\bcmd\bvim\0" },
+    config = { "\27LJ\2\0026\0\0\2\0\3\0\0056\0\0\0009\0\1\0'\1\2\0B\0\2\1K\0\1\0\23colorscheme embark\bcmd\bvim\0" },
     loaded = true,
     path = "/home/seanleishman/.local/share/nvim/site/pack/packer/start/embark",
     url = "https://github.com/embark-theme/vim"
@@ -175,7 +175,7 @@ _G.packer_plugins = {
 time([[Defining packer_plugins]], false)
 -- Config for: embark
 time([[Config for embark]], true)
-try_loadstring("\27LJ\2\n6\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\23colorscheme embark\bcmd\bvim\0", "config", "embark")
+try_loadstring("\27LJ\2\0026\0\0\2\0\3\0\0056\0\0\0009\0\1\0'\1\2\0B\0\2\1K\0\1\0\23colorscheme embark\bcmd\bvim\0", "config", "embark")
 time([[Config for embark]], false)
 
 _G._packer.inside_compile = false
