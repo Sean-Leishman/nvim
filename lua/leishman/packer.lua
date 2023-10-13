3a6329df7304eaa686439ffb8af466e5ca76a730
Sean Leishman <leishmansean@gmail.com>
2023-10-13 17:59:44 +0100

    Add spell check for .tex files

--- lua/leishman/packer.lua
+++ lua/leishman/packer.lua
@@ -18,6 +18,7 @@ return require('packer').startup(function(use)
         -- or                            , branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
+    use 'BurntSushi/ripgrep'
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use {
         'embark-theme/vim',
