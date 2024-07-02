2bf66ec018e7d0463de64c05c282892626312f68
Sean Leishman <leishmansean@gmail.com>
2024-07-02 21:45:14 +0100

    Add oh-my-posh

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -4,3 +4,4 @@ vim.keymap.set("i", "<S-TAB>", 'copilot#Accept("\\<CR>")', {
 })
 
 vim.g.copilot_no_tab_map = true
+vim.g.copilot_node_command = "/home/seanleishman/.config/nvm/versions/node/v20.13.1/bin/node"
