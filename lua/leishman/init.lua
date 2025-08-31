efe0a097d6d6c927a2715c7fffcf07289237a29f
Sean Leishman <s2051283@ed.ac.uk>
2025-08-31 13:27:29 +0100

    Update rempas and README.md to reflet

--- lua/leishman/init.lua
+++ lua/leishman/init.lua
@@ -2,3 +2,7 @@ require("leishman.remap")
 require("leishman.set")
 
 vim.api.nvim_set_option("clipboard", "unnamed")
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
--- lua/leishman/remap.lua
+++ lua/leishman/remap.lua
@@ -1,6 +1,11 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.keymap.set("n", "0", "$")
+vim.keymap.set("v", "0", "$")
+vim.keymap.set("n", "9", "0")
+vim.keymap.set("v", "9", "0")
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -13,3 +18,10 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "<C-c>", '"+y')
 vim.keymap.set("v", "<C-c>", '"+y')
 vim.keymap.set("n", "<C-c>c", '"+yy')
+
+vim.keymap.set('n', 'gV', '`[v`]', { noremap = true })
+vim.keymap.set('n', 'Y', 'y$', { noremap = true })
+
+vim.keymap.set("n", "<leader><leader>", "<c-^>")
+
+vim.keymap.set("n", "<leader><CR>", ":noh<CR>")
